3f304bb5764323daf672c2b5377eaf87292c342c
changwonyoon <cwyoon@semi-ts.com>
2024-01-09 09:40:39 +0900

    feat: initialize

---
+++ src/components/Sample.tsx
@@ -0,0 +1,13 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function Sample() {
+  return (
+    <>
+      test
+      <div className="hidden"></div>
+    </>
+  );
+}
+
+export default Sample;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  resolve: {
+    alias: {
+      $: fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '#': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '!': fileURLToPath(new URL('./src/libs', import.meta.url)),
+      src: fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: '/',
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: API,
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
 })
