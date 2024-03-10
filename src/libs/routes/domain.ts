93722fb023a41b15db79707d780940e191a88efb
uiwwsw <uiwwsw@icloud.com>
2024-03-10 12:54:05 +0900

    feat: remove form

--- src/libs/routes/domain.ts
+++ src/libs/routes/domain.ts
@@ -2,7 +2,6 @@ import { lazy } from 'react';
 
 const Button = lazy(() => import('src/pages/Button'));
 const Combo = lazy(() => import('src/pages/Combo'));
-const Form = lazy(() => import('src/pages/Form'));
 export const ROUTES = [
   {
     path: '/',
@@ -15,11 +14,5 @@ export const ROUTES = [
     auth: true,
     name: '콤보박스',
     node: Combo,
-  },
-  {
-    path: '/form',
-    auth: true,
-    name: '폼',
-    node: Form,
   },
 ];
